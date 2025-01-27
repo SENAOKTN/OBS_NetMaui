b3649876930e9bf515d645f1f4057cbe8f2cd659
SENAOKTN <119498087+SENAOKTN@users.noreply.github.com>
2025-01-27 20:29:37 +0300

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿  
+using obsSistemi.Sayfalar;
+using obsSistemi.Servis;
+
+namespace obsSistemi
+{
+    public partial class MainPage : ContentPage
+    {
+        OgrenciServis ogrenciServis =new OgrenciServis();
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnGiris_Clicked(object sender, EventArgs e)
+        {
+            var ogrNum = ogrenciNumara.Text;
+            var ogrSifre = ogrenciSifre.Text;
+
+
+            if(ogrNum == null || ogrSifre == null) {
+
+                await DisplayAlert("Hata", "Öğrenci numarası ve Şifre boş olamaz", "Tamam");
+            }
+            else
+            {
+                var sonuc = await ogrenciServis.Login(ogrNum, ogrSifre);
+                
+                await Navigation.PushAsync(new AnaSayfa());          
+            }
+        }
+    }
+}
